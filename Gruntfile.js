318097e9155781afef9617be423b2900d4453e47
huhm <506117084@qq.com>
2014-07-28 19:38:55 +0800

    生成配置

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+﻿module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        meta: {
+            banner: '/*! <%= pkg.name %> v<%= pkg.version %> | <%=pkg.author%> | <%= pkg.homepage %> */\n'
+        },
+        uglify: {
+            options: {
+                banner: '<%= meta.banner %>'
+            },
+            'validator.js': {
+                src: './src/validator.js',
+                dest: './dist/validator.js'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['uglify']);
+
+};
