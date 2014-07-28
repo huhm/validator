7f8adc05f06fd85d6fe316e1cf4f6a3cd5d2a28b
huhm <506117084@qq.com>
2014-07-28 19:26:45 +0800

    v1.0.0 源码

--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,517 @@
 ﻿define(function(require, exports, module) {
-    var FormHelper = require("./formhelper"),
-        buildInValidators = require("./common").Common, //内置校验
+     /**
+     * 常用合法性方法
+     * @return 正确返回true,错误返回string 错误信息
+     **/
+    var VALIDATE_METHOD = {
+        /**
+         * 手机号
+         */
+        telephone: function(val) {
+            return /^[1][0-9]{10}$/.test(val) ? true : "请输入11位手机号码";
+        },
+
+        /*
+         * 电话或手机号
+         */
+        contact: function(val) {
+            if (/^[1][0-9]{10}$/.test(val)) {
+                return true;
+            }
+            if (/^((\d{3,4})-(\d{7,8})|(\d{3,4})-(\d{7,8})-(\d{1,4}))$/.test(val)) {
+                return true;
+            }
+            return "请输入手机号码或电话号码(区号-电话号码[-分机号])";
+        },
+
+        /**
+         * 电话号码 "3-4位区号"+ "-"+  "7-8位直播号码"  +["-"+ "1－4位分机号"]
+         */
+        phone: function(val) {
+            return /^((\d{3,4})-(\d{7,8})|(\d{3,4})-(\d{7,8})-(\d{1,4}))$/.test(val) ? true : "电话号码格式：区号-电话号码[-分机号]";
+        },
+        ///**
+        //* 日期
+        //*/
+        //date: function (val) {
+        //    // matches mm/dd/yyyy (requires leading 0's (which may be a bit silly, what do you think?)
+        //    return /^(?:0[1-9]|1[0-2])\/(?:0[1-9]|[12][0-9]|3[01])\/(?:\d{4})/.test(val) ? true : "日期格式错误";
+        //},
+
+        /**
+         * 邮箱
+         */
+        email: function(val) {
+            return /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(val) ? true : "邮箱格式错误";
+        },
+
+        /**
+         * 最小长度
+         */
+        minLength: function(val, length) {
+            return val.length >= length || ("至少要有" + length + "个字符");
+        },
+        /**
+         * 最大长度
+         */
+        maxLength: function(val, length) {
+            return val.length <= length || ("最多不超过" + length + "个字符");
+        },
+
+        /**
+         * 最小字节数(有中文的情况)
+         */
+        minByteLength: function(val, length) {
+            var bLength = val.replace(/[^\x00-\xff]/g, 'xx').length;
+            return bLength >= length || ("至少要有" + length + "个字节");
+        },
+
+        /**
+         * 最大字节数(有中文的情况)
+         */
+        maxByteLength: function(val, length) {
+            var bLength = val.replace(/[^\x00-\xff]/g, 'xx').length;
+            return bLength <= length || ("最多不超过" + length + "个字节");
+        },
+
+        /**
+         * 整数,数字区间（parseInt后比较） ，十进制
+         * @param minMaxSet [1,99999]  [1,null] [null,99999]
+         */
+        numberRange: function(val, minMaxSet) {
+            //判断数字,可为负数
+            if (/^[-]?[0-9]+$/.test(val)) {
+                if (minMaxSet == null) {
+                    return true;
+                }
+                var number = parseInt(val, 10);
+                if (minMaxSet[0] != null && number < minMaxSet[0]) {
+                    return "不可小于" + minMaxSet[0];
+                }
+                if (minMaxSet[1] != null && number > minMaxSet[1]) {
+                    return "不可超过" + minMaxSet[1];
+                }
+                return true;
+            }
+
+            return "请输入整数";
+        },
+
+
+        /**
+         * 浮点型数据区间（parseFloat后比较） ，十进制
+         * @param minMaxSet [1,99999]  [1,null] [null,99999]
+         */
+        floatRange: function(val, minMaxSet) {
+            //判断数字,可为负数
+            if (/^[-]?[0-9]+[.]?[0-9]*$/.test(val)) { //Error
+                if (minMaxSet == null) {
+                    return true;
+                }
+                var floatNumber = parseFloat(val, 10);
+                if (minMaxSet[0] != null && floatNumber < minMaxSet[0]) {
+                    return "不可小于" + minMaxSet[0];
+                }
+                if (minMaxSet[1] != null && floatNumber > minMaxSet[1]) {
+                    return "不可超过" + minMaxSet[1];
+                }
+                return true;
+            }
+            if (minMaxSet == null) {
+                return "请输入浮点型数据";
+            }
+            return "请输入范围为[ " +
+                (minMaxSet[0] == null ? "--" : minMaxSet[0]) +
+                " , " +
+                (minMaxSet[1] == null ? "--" : minMaxSet[1]) +
+                " ]浮点型数据";
+
+        },
+
+        /*
+         * 浮点精度判断
+         * @param {Array} minMaxSet [min,max]
+         *               小数点后的位数范围,为null时没有限制
+         */
+        floatPrecision: function(strVal, minMaxSet) {
+            //判断数字,可为负数
+            if (/^[-]?[0-9]+[.]?[0-9]*$/.test(strVal)) { //Error
+                if (minMaxSet == null) {
+                    return true;
+                }
+                var idx = strVal.indexOf("."),
+                    decimalCnt = 0,
+                    numCnt = strVal.length;
+                if (idx >= 0) {
+                    decimalCnt = numCnt - idx - 1;
+                    numCnt = idx;
+                }
+                if (minMaxSet[0] != null) {
+                    if (decimalCnt < minMaxSet[0]) {
+                        return "至少要有" + minMaxSet[0] + "位小数";
+                    }
+                }
+                if (minMaxSet[1] != null) {
+                    if (decimalCnt > minMaxSet[1]) {
+                        return "小数位不能超过" + minMaxSet[1] + "位";
+                    }
+                }
+                return true;
+            }
+            if (minMaxSet == null) {
+                return "请输入浮点型数据";
+            }
+            return "请输入小数位数为" + (minMaxSet[0] || "0") + " ~ " + (minMaxSet[1] || "--") + "位的浮点型数据";
+        },
+
+        /**
+         * 相等
+         */
+        equal: function(val, val2) {
+            return (val == val2) || "应输入" + val2;
+        },
+
+        /**
+         * 不相等
+         */
+        notEqual: function(val, val2) {
+            return (val != val2) || "值不能为" + val2;
+        },
+
+        /**
+         * 名称通用
+         * 中文、字母、数字、下划线
+         */
+        name: function(val) { //空格、-
+            return /^([\u4E00-\uFA29]|[\uE7C7-\uE7F3]|[a-zA-Z0-9]|_)*$/.test(val) || "只能由中文、字母、数字、下划线组成";
+        },
+        ///**
+        //* 选择项项数（val="val1,val2"）
+        //* @note 各option值不可有','否则会出错
+        //*/
+        //maxCheckboxCount: function (val, maxCount) {
+        //    var ary = val.split(",");
+        //    return (val == "" || ary.length <= maxCount) || ("不可超过" + maxCount + "项");
+        //},
+        //minCheckboxCount: function (val, minCount) {
+        //    var ary = val.split(",");
+        //    return (val !== "" && ary.length >= minCount) || ("不可低于" + minCount + "项");
+
+        //},
+
+        //[minCount,maxCount],[minCount,null],[null,maxCount]
+        //minCount,maxCount为null时，不检查,
+        checkboxCount: function(val, minMaxSet) {
+            if (minMaxSet == null) {
+                return true;
+            }
+            var ary = val.split(",");
+            if (minMaxSet[0] != null) {
+                if (val == "" || ary.length < minMaxSet[0]) {
+                    return ("不可低于" + minMaxSet[0] + "项");
+                }
+            }
+            if (minMaxSet[1] != null) {
+
+                if (ary.length > minMaxSet[1]) {
+                    return ("不可超过" + minMaxSet[1] + "项");
+                }
+            }
+            return true;
+        }
+    };
+
+    exports.Common = VALIDATE_METHOD;
+
+    var Browser = {
+        isIE: !! window.ActiveXObject
+    };
+    if (Browser.isIE) {
+        if (document.documentMode == 10)
+            Browser.IE = 10;
+        else if (document.documentMode == 9)
+            Browser.IE = 9;
+        else if (window.postMessage)
+            Browser.IE = 8;
+        else if (window.XMLHttpRequest)
+            Browser.IE = 7;
+        else if (document.compatMode)
+            Browser.IE = 6;
+        else if (window.createPopup)
+            Browser.IE = 5.5;
+        else if (window.attachEvent)
+            Browser.IE = 5;
+        else if (document.all) {
+            Browser.IE = 4;
+        } else {
+            Browser.IE = 0;
+        }
+    }
+
+
+    /**
+     * 表单帮助类
+     */
+    var FormHelper = function(container) {
+        container = $(container);
+        this.container = container;
+    };
+
+    /*
+     * 获取输入对象
+     */
+    FormHelper.prototype.GetInputDom = function(itemName) {
+        return this.container.find("[name='" + itemName + "']");
+    };
+
+    //获取[name=itemName]的元素type @type {String:name属性值|Jquery对象} nameOrJqDomObj
+    FormHelper.prototype._GetInputTypeByDom = function(nameOrJqDomObj) {
+        //根据InputElement确定type
+        var inputEle;
+        if (typeof nameOrJqDomObj === "string") {
+            inputEle = this.GetInputDom(nameOrJqDomObj);
+        } else { //Jquery对象
+            inputEle = nameOrJqDomObj;
+        }
+        inputEle = inputEle.first();
+        if (inputEle.length === 0) {
+            return null;
+        } else {
+            switch (inputEle[0].tagName.toLowerCase()) { //以第0个元素的tagName为准
+                case "input":
+                    return {
+                        "checkbox": "checkbox",
+                        "radio": "radio",
+                        "hidden": "hidden",
+                        "file": "file",
+                        "text": "text",
+                        "password": "password"
+                    }[inputEle.attr("type")] || "input"; //checkbox,radio,hidden,text,password,file....
+                case "select":
+                    return "select";
+                case "textarea":
+                    return "textarea";
+                default: //static
+                    return "static"; //普通文本
+            }
+
+        }
+    };
+
+    //获取[name=itemName]的元素值,不一定是要校验的元素
+    FormHelper.prototype.GetValue = function(itemName) {
+        var inputElement = this.GetInputDom(itemName);
+        return this._GetValue(inputElement, true);
+    };
+    //获取select的text值
+    FormHelper.prototype.GetText = function(itemName) {
+        var inputElement = this.GetInputDom(itemName);
+        if (inputElement == null || inputElement.length === 0) {
+            return null;
+        }
+        var type = this._GetInputTypeByDom(inputElement);
+        switch (type) {
+            case "select": //防止disabled 获取失败
+                return inputElement.find("option:selected").text();
+                //返回选择的所有值（用,分隔）         
+                //case "checkbox": //ui-form-controls
+                //case "radio": //ui-form-controls
+                //    var checkedbox = inputElement.filter(":checked");
+                //    if (inputElement.length > 0) {
+                //        var valAry = [];
+                //        checkedbox.each(function (idx, item) {
+                //            valAry.push($(item).val());
+                //        });
+                //        return valAry.join(",");
+                //    }
+                //    else {
+                //        return null;
+                //   }
+                //   break; 
+        }
+        return null;
+    }
+
+    //获取[name=itemName]的元素值,不一定是要校验的元素
+    FormHelper.prototype._GetValue = function(inputElement, _bIncludeAllEle) {
+        var type = this._GetInputTypeByDom(inputElement);
+        if (inputElement == null || inputElement.length === 0) {
+            return null;
+        }
+        switch (type) {
+            case "static":
+                return inputElement.html(); //text()改为html,可自定义innerHtml
+            case "select": //防止disabled 获取失败
+                return inputElement.find("option:selected").val();
+                //返回选择的所有值（用,分隔）         
+            case "checkbox": //ui-form-controls
+            case "radio": //ui-form-controls
+                if (!_bIncludeAllEle) {
+                    inputElement = this.GetInputDom(inputElement.attr("name"));
+                }
+                var checkedbox = inputElement.filter(":checked");
+                if (inputElement.length > 0) {
+                    var valAry = [];
+                    checkedbox.each(function(idx, item) {
+                        valAry.push($(item).val());
+                    });
+                    return valAry.join(",");
+                } else {
+                    return null;
+                }
+                break;
+            case "input":
+            case "text":
+            case "file":
+            case "hidden":
+            case "password":
+            case "textarea":
+                return inputElement.val();
+            default:
+                window.console &&
+                    window.console.warn &&
+                    window.console.warn("Validator:未定义类型：" +
+                        type + ",使用默认获取方式$ele.val();");
+                return inputElement.val();
+        }
+    };
+
+    /*获取表单中[name]的元素值,不一定是要校验的元素 
+     * @return {Name:VALUE,...}
+     */
+    FormHelper.prototype.GetValues = function() {
+        var inputDoms = this.container.find('[name]'),
+            res = {};
+        for (var i = 0, len = inputDoms.length; i < len; i++) {
+            var ele = $(inputDoms[i]),
+                name = ele.attr("name");
+            if (res[name] === undefined) {
+                res[ele.attr("name")] = this._GetValue(ele, false);
+            }
+        }
+        return res;
+    };
+
+    /*
+     * 设置form字段的值(可设置非校验项值)
+     * @param {Bool|Null} True:触发表单项校验，默认:不触发
+     */
+    FormHelper.prototype.SetValue = function(itemName, newValue, bTriggerValidateOrTriggerEvent) {
+        var inputEle = this.GetInputDom(itemName),
+            type = this._GetInputTypeByDom(itemName);
+        if (inputEle == null || inputEle.length === 0) {
+            return false;
+        }
+        if (newValue == null) {
+            newValue = "";
+        };
+        var valueType = typeof newValue;
+        if (typeof newValue !== "string" && valueType !== "number") {
+
+            window.console &&
+                window.console.warn &&
+                window.console.warn("非string|number值不支持：" +
+                    itemName + ",value=" + newValue);
+            return;
+        }
+        switch (type) {
+            case "static":
+                inputEle.html(newValue); //text()改为html,可自定义innerHtml
+                break;
+            case "checkbox":
+                //if (typeof newValue === "string") {
+                //#mod2
+                var val2 = "," + newValue + ",";
+                inputEle.each(function() {
+                    if (val2.indexOf("," + $(this).val() + ",") >= 0) {
+                        this.checked = true;
+                    } else {
+                        this.checked = false;
+                    }
+                });
+                //var valAry = newValue.split(",");
+                //inputEle.removeAttr("checked");
+                //if (valAry) {
+                //    for (var i = 0, len = valAry.length; i < len; i++) {
+                //        inputEle.filter('[value="' + valAry[i] + '"]').
+                //            attr("checked", "checked");
+                //    }
+                //}
+                //}
+                //else {
+                //    window.console &&
+                //    window.console.warn &&
+                //    window.console.warn("checkbox 非string值不支持：" +
+                //        itemName + ",value="+newValue); 
+                //throw "checkbox 非string值不支持";
+                //}
+                break;
+            case "radio":
+                inputEle.each(function() {
+                    if ($(this).val() == newValue) {
+                        this.checked = true;
+                    } else {
+                        this.checked = false;
+                    }
+                });
+                break;
+            case "input":
+            case "text":
+            case "hidden":
+            case "password":
+            case "textarea":
+                inputEle.val(newValue);
+                break;
+            case "select": //防止disabled时,获取失败
+                if (Browser.isIE && Browser.IE < 7) { //IE6 以下  select 无法设置selected错误
+                    var $op = inputEle.find('option[value="' + newValue + '"]');
+                    setTimeout(function() {
+                        $op.attr("selected", true);
+                        dealEventTrigger();
+                    }, 1);
+                    return inputEle; 
+                } else {
+                    inputEle.val(newValue); 
+                }
+                break;
+            case "file": //doNothing
+                if (!newValue) { //清空
+                    inputEle.val("");
+                }
+                break;
+            default:
+                window.console &&
+                    window.console.warn &&
+                    window.console.warn("Validator:未定义类型：" +
+                        type + ",使用默认设置值方式$ele.val(newValue);");
+                inputEle.val(newValue);
+                break;
+        }
+        //处理事件触发
+        function dealEventTrigger(){ 
+            if (bTriggerValidateOrTriggerEvent) {
+                if(typeof bTriggerValidateOrTriggerEvent!=="string"){ 
+                    bTriggerValidateOrTriggerEvent="validate";
+                }
+                inputEle.trigger(bTriggerValidateOrTriggerEvent);
+            }
+        }
+        dealEventTrigger();
+        return inputEle;
+    };
+
+    /*
+     * 设置表单字段值(可设置非校验项值)
+     * @param {Bool|Null} True:触发表单项校验
+     */
+    FormHelper.prototype.SetValues = function(valueObj, bTriggerValidateOrTriggerEvent) {
+        for (var key in valueObj) {
+            this.SetValue(key, valueObj[key], bTriggerValidateOrTriggerEvent);
+        }
+    };
+
+    
+    var buildInValidators = VALIDATE_METHOD, //内置校验
         DefaultValidateOption = { //默认校验选项
             /*要校验的input对象name [NotNull]
              */
